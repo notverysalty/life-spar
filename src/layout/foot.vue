0e01317173735fa5d880ef65524cca8c43b5a964
notverysalty <1197181269@qq.com>
2019-11-20 17:42:12 +0800

    增加顶部通栏

---
+++ src/layout/foot.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer class="foot">
+    <ul>
+      <li>
+        <img src="@/assets/Images/ewm.jpg" alt />
+        <div>
+          <h3>官方微信公众号</h3>
+          <p>扫描二维码，关注官方微信，殡葬习俗讲究不用愁！</p>
+        </div>
+      </li>
+      <li>
+        <img src="@/assets/Images/fottel.jpg" alt />
+        <div>
+          <h2>18079116161</h2>
+          <p>拨打服务热线咨询详情</p>
+        </div>
+      </li>
+    </ul>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .foot {
+    background-image: url('@/assets/Images/fotbg.jpg')
+  }
+</style>
---
+++ src/layout/head.vue
@@ -0,0 +1,62 @@
+<template>
+  <header class="head">
+    <div class="top">
+      <div class="logo-left">
+        <img src="@/assets/Images/logo.png" alt />
+      </div>
+      <div class="logo-right">
+        <img src="@/assets/Images/logotel.png" alt />
+        <span>18079116161</span>
+      </div>
+    </div>
+    <div class="carousel">
+      <el-carousel height="550px">
+        <el-carousel-item v-for="item in image" :key="item">
+          <img style="width: 100%; height: 100%;" src="@/assets/Images/lb2.jpg"/>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      image: ['@/assets/Images/lb1.jpg', '@/assets/Images/lb2.jpg']
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.head {
+  .top {
+    width: 60%;
+    margin: 0 auto;
+    height: 60px;
+    padding: 15px 0;
+    overflow: hidden;
+    .logo-left {
+      float: left;
+      img {
+        height: 60px;
+      }
+    }
+    .logo-right {
+      float: right;
+      img {
+        height: 60px;
+      }
+      span {
+        position: absolute;
+        right: 405px;
+        top: 39px;
+        font-size: 20px;
+        font-weight: bold;
+        letter-spacing: 1px;
+      }
+    }
+  }
+}
+</style>
