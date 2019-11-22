fe37dd582247f41b1f74bc30c3c916d196998e32
notverysalty <1197181269@qq.com>
2019-11-22 16:02:30 +0800

    修改导航栏细节

--- src/layout/navMenu.vue
+++ src/layout/navMenu.vue
@@ -1,13 +1,20 @@
 <template>
-  <el-menu default-active="1" active-text-color="#1C5127" class="el-menu-demo navMenu" mode="horizontal">
-    <el-menu-item index="1">首页</el-menu-item>
-    <el-menu-item index="2">陵园墓地</el-menu-item>
-    <el-menu-item index="3">生命晶石</el-menu-item>
-    <el-menu-item index="4">周边产品</el-menu-item>
-    <el-menu-item index="5">族谱</el-menu-item>
-    <el-menu-item index="6">新闻资讯</el-menu-item>
-    <el-menu-item index="7">关于我们</el-menu-item>
-  </el-menu>
+  <div class="nav">
+    <el-menu
+      default-active="1"
+      active-text-color="#1C5127"
+      class="el-menu-demo navMenu"
+      mode="horizontal"
+    >
+      <el-menu-item index="1">首页</el-menu-item>
+      <el-menu-item index="2">陵园墓地</el-menu-item>
+      <el-menu-item index="3">生命晶石</el-menu-item>
+      <el-menu-item index="4">周边产品</el-menu-item>
+      <el-menu-item index="5">族谱</el-menu-item>
+      <el-menu-item index="6">新闻资讯</el-menu-item>
+      <el-menu-item index="7">关于我们</el-menu-item>
+    </el-menu>
+  </div>
 </template>
 
 <script>
@@ -15,9 +22,14 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
+.nav {
+  width: 100%;
+  background-color: #fff;
   .navMenu {
-    padding: 0 30%;
+    margin: auto;
     box-sizing: border-box;
-    width: 100%;
+    border-bottom: none;
+    width: 1200px;
   }
+}
 </style>
